28fce905ee16e38a9772cccf730584cb8edb2a91
madchan <madchan912@gmail.com>
2024-10-30 22:55:39 +0900

    PostgreSQL 예약어 오류로 인한 수정

--- src/main/java/com/sparta/ecommerce/entity/User.java
+++ src/main/java/com/sparta/ecommerce/entity/User.java
@@ -1,12 +1,10 @@
 package com.sparta.ecommerce.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Table(name = "app_user") // 테이블 이름을 "app_user"로 변경
 @Data
 public class User {
     @Id
