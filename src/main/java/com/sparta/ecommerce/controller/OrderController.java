fbf1bf226debd58661bb710028803c1b6276eb03
madchan <madchan912@gmail.com>
2024-10-30 22:31:40 +0900

    Controller 생성

---
+++ src/main/java/com/sparta/ecommerce/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.sparta.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Order Controller is working!";
+    }
+}
---
+++ src/main/java/com/sparta/ecommerce/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.sparta.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Product Controller is working!";
+    }
+}
