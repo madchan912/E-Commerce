12501943fff593deaa75c8aa19cb7b8502d53b09
madchan <madchan912@gmail.com>
2024-12-21 04:13:12 +0900

    각 로그 추가 및 오더서비스 에러 수정

--- order-service/src/main/java/com/sparta/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/sparta/orderservice/OrderServiceApplication.java
@@ -3,7 +3,7 @@ package com.sparta.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.sparta.orderservice", "com.sparta.common"})
+@SpringBootApplication(scanBasePackages = {"com.sparta.orderservice", "com.sparta.userservice", "com.sparta.productservice", "com.sparta.common"})
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
